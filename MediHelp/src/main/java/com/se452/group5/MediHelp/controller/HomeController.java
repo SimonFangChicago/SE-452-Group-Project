65fbd29163dbbd022d830c502315b94bab0c4a1e
SimonFangChicago <47611549+SimonFangChicago@users.noreply.github.com>
2020-05-16 19:41:52 -0500

    Add home page.

---
+++ MediHelp/src/main/java/com/se452/group5/MediHelp/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.se452.group5.MediHelp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+  
+    @GetMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
+    
+}
