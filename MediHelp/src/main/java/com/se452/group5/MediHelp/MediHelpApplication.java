ed94346a6c8ae915df421a20f9fb74419a05e183
SimonFangChicago <47611549+SimonFangChicago@users.noreply.github.com>
2020-05-17 19:41:15 -0500

    Fixed return html not working

--- MediHelp/src/main/java/com/se452/group5/MediHelp/MediHelpApplication.java
+++ MediHelp/src/main/java/com/se452/group5/MediHelp/MediHelpApplication.java
@@ -1,23 +1,14 @@
 package com.se452.group5.MediHelp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 @SpringBootApplication
-public class MediHelpApplication /*implements CommandLineRunner */{
-    //@Autowired
-    //JdbcTemplate jdbcTemplate;
+public class MediHelpApplication{
+
     
     public static void main(String[] args) {
         SpringApplication.run(MediHelpApplication.class, args);
     }
-    /*public void run(String... args) throws Exception {
-        System.out.println("Reading Patient records...");
-        //System.out.printf("%-30.30s  %-30.30s%n", "Title", "Description");
-        jdbcTemplate.query("SELECT * FROM Patient", (rs)-> {
-            System.out.printf("%-30.30s  %-30.30s%n", rs.getString("PATIENTNAME"), rs.getString("PATIENTGENDER"));          
-        });
-    }*/
+
 }
--- MediHelp/src/main/java/com/se452/group5/MediHelp/controller/HomeController.java
+++ MediHelp/src/main/java/com/se452/group5/MediHelp/controller/HomeController.java
@@ -1,15 +1,14 @@
 package com.se452.group5.MediHelp.controller;
 
-import javax.sql.DataSource;
-
 import com.se452.group5.MediHelp.model.Patient;
 import com.se452.group5.MediHelp.repository.PatientRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @Autowired
@@ -27,6 +26,13 @@ public class HomeController {
 
         return result;
         //return result;
+    }
+
+    @GetMapping("/test")
+    public String hello(Model model){
+
+        model.addAttribute("name","simon");
+        return "hello";
     }
     
 }
